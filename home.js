0799bf25622e398c83be14941d13aa122a40f2b6
Aayush Kumar Fabwani <111650763+aaayush2604@users.noreply.github.com>
2024-01-10 20:00:21 +0530

    Update home.js
    API key hidden

--- home.js
+++ home.js
@@ -1,4 +1,4 @@
-const apiKey = "3cb981ebeb249a4d9db88e08df13d82e";
+const apiKey = "xxxxxxxxxxxxxxxxxxxxxxxxxxxxx";
 const apiURL =
   "https://api.openweathermap.org/data/2.5/weather?units=metric&q=";
 const searchBox = document.querySelector(".search input");
